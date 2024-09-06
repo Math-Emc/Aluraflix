1bd76eb4dee6f1202f7f076415baf89d6c171344
Math-Emc <00001137713082sp@al.educacao.sp.gov.br>
2024-09-06 00:10:29 -0300

    tirei os códigos do index.js

--- index.js
+++ index.js
@@ -17,12 +17,3 @@ var batsu = document.createElement("input");
     batsu.value = 'AGAIN';  
     batsu.onclick = function() { window.history.go(0); };  
     document.body.appendChild(batsu);
-
-<button id="red">Vermelho</button>
-<button id="green">Verde</button>
-<button id="blue">Azul</button>
-<script type="text/javascript">
-    $('button').click(function(){
-        $('body').css('background-color',$(this).attr('id'));
-    });
-</script>
